46d00615c94219525c8fae3de2eb6ed7d1340601
ntotten-kainos <nathan.totten@kainos.com>
2024-11-28 13:36:25 +0000

    Initial scaffolding work

---
+++ src/JobApplicationMVCApp/Controllers/JobsController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JobApplicationMVCApp.Data;
+using JobApplicationMVCApp.Models;
+
+namespace JobApplicationMVCApp.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jobs
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.JobPostings.Include(j => j.Department).Include(j => j.Location);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // // GET: Jobs/Details/5
+        // public async Task<IActionResult> Details(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     var jobPosting = await _context.JobPostings
+        //         .Include(j => j.Department)
+        //         .Include(j => j.Location)
+        //         .FirstOrDefaultAsync(m => m.JobPostingId == id);
+        //     if (jobPosting == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     return View(jobPosting);
+        // }
+
+        // GET: Jobs/Create
+        public IActionResult Create()
+        {
+            ViewData["JobDepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName");
+            ViewData["JobLocationId"] = new SelectList(_context.Locations, "LocationId", "LocationCity");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("JobPostingId,JobTitle,JobDescription,JobRequirements,JobLocationId,JobDepartmentId,Salary,ClosingDate,Type,Status,DatePosted")] JobPosting jobPosting)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jobPosting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JobDepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName", jobPosting.JobDepartmentId);
+            ViewData["JobLocationId"] = new SelectList(_context.Locations, "LocationId", "LocationCity", jobPosting.JobLocationId);
+            return View(jobPosting);
+        }
+
+        // // GET: Jobs/Edit/5
+        // public async Task<IActionResult> Edit(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     var jobPosting = await _context.JobPostings.FindAsync(id);
+        //     if (jobPosting == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     ViewData["JobDepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName", jobPosting.JobDepartmentId);
+        //     ViewData["JobLocationId"] = new SelectList(_context.Locations, "LocationId", "LocationCity", jobPosting.JobLocationId);
+        //     return View(jobPosting);
+        // }
+
+        // // POST: Jobs/Edit/5
+        // // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> Edit(int id, [Bind("JobPostingId,JobTitle,JobDescription,JobRequirements,JobLocationId,JobDepartmentId,Salary,ClosingDate,Type,Status,DatePosted")] JobPosting jobPosting)
+        // {
+        //     if (id != jobPosting.JobPostingId)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     if (ModelState.IsValid)
+        //     {
+        //         try
+        //         {
+        //             _context.Update(jobPosting);
+        //             await _context.SaveChangesAsync();
+        //         }
+        //         catch (DbUpdateConcurrencyException)
+        //         {
+        //             if (!JobPostingExists(jobPosting.JobPostingId))
+        //             {
+        //                 return NotFound();
+        //             }
+        //             else
+        //             {
+        //                 throw;
+        //             }
+        //         }
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     ViewData["JobDepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentName", jobPosting.JobDepartmentId);
+        //     ViewData["JobLocationId"] = new SelectList(_context.Locations, "LocationId", "LocationCity", jobPosting.JobLocationId);
+        //     return View(jobPosting);
+        // }
+
+        // // GET: Jobs/Delete/5
+        // public async Task<IActionResult> Delete(int? id)
+        // {
+        //     if (id == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     var jobPosting = await _context.JobPostings
+        //         .Include(j => j.Department)
+        //         .Include(j => j.Location)
+        //         .FirstOrDefaultAsync(m => m.JobPostingId == id);
+        //     if (jobPosting == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     return View(jobPosting);
+        // }
+
+        // // POST: Jobs/Delete/5
+        // [HttpPost, ActionName("Delete")]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> DeleteConfirmed(int id)
+        // {
+        //     var jobPosting = await _context.JobPostings.FindAsync(id);
+        //     if (jobPosting != null)
+        //     {
+        //         _context.JobPostings.Remove(jobPosting);
+        //     }
+        //
+        //     await _context.SaveChangesAsync();
+        //     return RedirectToAction(nameof(Index));
+        // }
+
+        private bool JobPostingExists(int id)
+        {
+            return _context.JobPostings.Any(e => e.JobPostingId == id);
+        }
+    }
+}
---
+++ src/JobApplicationMVCApp/Migrations/20241128133249_CreatedModelsForJobPosting.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobApplicationMVCApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedModelsForJobPosting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DepartmentName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LocationName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LocationStreetAddress = table.Column<string>(type: "varchar(171)", maxLength: 171, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LocationCity = table.Column<string>(type: "varchar(58)", maxLength: 58, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LocationPostCode = table.Column<string>(type: "varchar(7)", maxLength: 7, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LocationCountry = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "JobPostings",
+                columns: table => new
+                {
+                    JobPostingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    JobTitle = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    JobDescription = table.Column<string>(type: "varchar(1000)", maxLength: 1000, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    JobRequirements = table.Column<string>(type: "varchar(1000)", maxLength: 1000, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    JobLocationId = table.Column<int>(type: "int", nullable: false),
+                    JobDepartmentId = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<double>(type: "double", nullable: true),
+                    ClosingDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DatePosted = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobPostings", x => x.JobPostingId);
+                    table.ForeignKey(
+                        name: "FK_JobPostings_Departments_JobDepartmentId",
+                        column: x => x.JobDepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobPostings_Locations_JobLocationId",
+                        column: x => x.JobLocationId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_JobDepartmentId",
+                table: "JobPostings",
+                column: "JobDepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_JobLocationId",
+                table: "JobPostings",
+                column: "JobLocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobPostings");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
